5f2b056946ca40e322613789e6dbf2e99c3717e4
Pal987 <51531208+Pal987@users.noreply.github.com>
2024-07-24 17:42:14 +0530

    Update TestCalculator.java

--- String Calculator/src/TestCalculator.java
+++ String Calculator/src/TestCalculator.java
@@ -2,6 +2,11 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static org.testng.AssertJUnit.assertEquals;
+//	Task - 1: Create a simple String calculator with a method signature:
+	//	———————————————
+	//	int Add(string numbers)
+	//	———————————————
+	// TODO: for empty string it will return zero
 
 @Test
 public class TestCalculator {
@@ -11,25 +16,32 @@ public class TestCalculator {
     {
         calculator=new Calculator();
     }
+    // An empty string returns zero
     public void emptyStringReturnsZero() throws Exception {
         assertEquals(calculator.calculate(""),0);
     }
+    // A single number returns the value
     public void singleValueIsReplied() throws Exception {
      assertEquals(calculator.calculate("1"),1);
     }
+    //Two numbers, comma delimited, returns the sum
     public void twoNumbersCommaDelimitedReturnSum() throws Exception {
         assertEquals(calculator.calculate("1,2"), 3);
     }
+    //Two numbers, newline delimited, returns the sum
  public void twoNumbersNewLineDelimitedReturnSum() throws Exception {
         assertEquals(calculator.calculate("1\n2"),3);
  }
+    //Three numbers, delimited either way, returns the sum
  public void threeNumbersNewLineDelimitedReturnSum() throws Exception {
         assertEquals(calculator.calculate("1,2,3"),6);
  }
+    //Negative numbers throw an exception
  @Test(expectedExceptions = Exception.class)
     public void negativeInputReturnsException() throws Exception {
         calculator.calculate("-1");
  }
+    // Numbers greater than 1000 are ignored
  public void ignoreNumbersGreaterThan1000() throws Exception {
         assertEquals(calculator.calculate("10,10,1001"),20);
  }
